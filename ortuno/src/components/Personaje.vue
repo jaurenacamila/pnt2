49157403996e8eebda6d986184452863bb4c6f7b
Camila Jaurena <jaurenacamila@gmail.com>
2023-05-02 15:55:52 -0300

    primer proyecto vue

---
+++ ortuno/src/components/Personaje.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+        count: 0
+        }
+    },
+    methods: {
+        saludar() {
+            alert('Hola ' + this.count)
+        }
+    }
+}
+</script>
+<template>
+    <input type="button" value="Personaje">
+    <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
+<style scoped>
+</style>
